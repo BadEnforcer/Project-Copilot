4aa20a543892c702ba224a02c0a14af27c26690d
badenforcer <rajdwivedi008@outlook.com>
2022-10-31 23:20:57 +0530

    Second commit. added a todos.py

---
+++ todos.py
@@ -0,0 +1,24 @@
+# TODO: Setup Basic Webpages
+# TODO: Setup Database for Challenges, UsersData
+# TODO: Setup User Authentication
+# TODO: Setup Dynamic Content loading
+# TODO: Setup redirecting
+# TODO: Setup Admin panel
+# TODO: Setup Data-Trackers
+# TODO: Setup Code Editor window
+# TODO: IMPORTANT : How to make the code run on client side ?
+# TODO: Setup the Python Code Runner
+# TODO: Enable Code protection to stop malicious code from running.
+# TODO: Setup Answer Checker
+# TODO: Setup User Ban System
+# TODO: Setup Bug reporting System
+# TODO: Setup Support Backend
+# TODO: Create a Solutions Library
+# TODO: Setup Audio alerts ?*
+# TODO: Add Performance Trackers ?
+# TODO: Add Paid Features ?*
+# TODO: Setup Forget-password mechanism, 2-Factor Authentication
+# TODO: Support Dark Theme ? *
+# TODO: Setup Achievements
+# TODO: Setup News Page
+# TODO: Setup Keyboard-Shortcuts
